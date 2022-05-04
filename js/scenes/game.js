d67164e857f009a6121a658ea65dd880a27775b1
inakilbss <inakilbss@gmail.com>
2022-05-04 23:21:38 +0200

    Cards are shown after making a mistake

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -3,6 +3,7 @@ class GameScene extends Phaser.Scene {
         super('GameScene');
 		this.cards = null;
 		this.firstClick = null;
+		this.secondClick = null;
 		this.score = 100;
 		this.correct = 0;
     }
@@ -17,22 +18,42 @@ class GameScene extends Phaser.Scene {
 		this.load.image('to', '../resources/to.png');
 	}
 	
-    create (){	
-		let arraycards = ['co', 'sb', 'co', 'sb'];
+    create (){
+		var json = localStorage.getItem("config") || '{"cards":2,"difficulty":"hard"}';
+		this.options_data = JSON.parse(json);
+
+		let arraycards = ['co', 'co', 'sb', 'sb', 'so', 'so', 'tb', 'tb'].slice (0, this.options_data.cards * 2);
+		console.log (arraycards);
 		arraycards.sort (function() {return Math.random() - 0.5});
 		this.cameras.main.setBackgroundColor(0xBFFCFF);
 		
-		this.add.image(250, 300, arraycards[0]);
-		this.add.image(350, 300, arraycards[1]);
-		this.add.image(450, 300, arraycards[2]);
-		this.add.image(550, 300, arraycards[3]);
-		
+		this.add.image(300, 200, arraycards[0]);
+		this.add.image(300, 400, arraycards[1]);
+		this.add.image(500, 200, arraycards[2]);
+		this.add.image(500, 400, arraycards[3]);
+		if (this.options_data.cards >= 3) {
+			this.add.image(200, 300, arraycards[4]);
+			this.add.image(600, 300, arraycards[5]);
+			if (this.options_data.cards >= 4) {
+				this.add.image(400, 100, arraycards[6]);
+				this.add.image(400, 500, arraycards[7]);
+			}
+		}
+
 		this.cards = this.physics.add.staticGroup();
 		
-		this.cards.create(250, 300, 'back');
-		this.cards.create(350, 300, 'back');
-		this.cards.create(450, 300, 'back');
-		this.cards.create(550, 300, 'back');
+		this.cards.create(300, 200, 'back');
+		this.cards.create(300, 400, 'back');
+		this.cards.create(500, 200, 'back');
+		this.cards.create(500, 400, 'back');
+		if (this.options_data.cards >= 3) {
+			this.cards.create(200, 300, 'back');
+			this.cards.create(600, 300, 'back');
+			if (this.options_data.cards >= 4) {
+				this.cards.create(400, 100, 'back');
+				this.cards.create(400, 500, 'back');
+			}
+		}
 		
 		let i = 0;
 		this.cards.children.iterate((card)=>{
@@ -41,24 +62,30 @@ class GameScene extends Phaser.Scene {
 			card.setInteractive();
 			card.on('pointerup', () => {
 				card.disableBody(true,true);
-				if (this.firstClick){
+				if (this.secondClick) {
+					card.enableBody(false, 0, 0, true, true);
+					this.firstClick.enableBody(false, 0, 0, true, true);
+					this.secondClick.enableBody(false, 0, 0, true, true);
+					this.firstClick = null;
+					this.secondClick = null;
+				}
+				else if (this.firstClick){
 					if (this.firstClick.card_id !== card.card_id){
 						this.score -= 20;
-						this.firstClick.enableBody(false, 0, 0, true, true);
-						card.enableBody(false, 0, 0, true, true);
 						if (this.score <= 0){
 							alert("Game Over");
 							loadpage("../");
 						}
+						this.secondClick = card;
 					}
 					else{
 						this.correct++;
-						if (this.correct >= 2){
+						if (this.correct >= this.options_data.cards){
 							alert("You Win with " + this.score + " points.");
 							loadpage("../");
 						}
+						this.firstClick = null;
 					}
-					this.firstClick = null;
 				}
 				else{
 					this.firstClick = card;
